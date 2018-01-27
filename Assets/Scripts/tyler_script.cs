eb3975fa8fcc453b9084a64ac4a5d9e9987efcb1
Tyler Witten <turtlewit@live.com>
2018-01-27 14:45:06 -0800

    finish movement script

---
+++ Assets/Scripts/tyler_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tyler_script : MonoBehaviour {
+
+	public float speed;
+	Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement (){
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, speed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, -1 * speed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-1 * speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
