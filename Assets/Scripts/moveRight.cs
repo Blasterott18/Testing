7c3c24d6c84960fed422e3ef409be7923186635c
Blasterott18 <blasterott18@yahoo.com>
2018-01-27 16:27:50 -0800

    Add rightwards movement in Scene1

---
+++ Assets/Scripts/moveRight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+		
+	}
+
+	void movement (){
+		transform.Translate (10 * Time.deltaTime, 0, 0);
+	}
+}
